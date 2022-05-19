cf9842c49b54a1c572323dd612eec96ec912ab01
Роман <78904284+cruelthem@users.noreply.github.com>
2022-05-19 16:22:50 +0300

    Update tgBotTest.py

--- tgBotTest.py
+++ tgBotTest.py
@@ -1,20 +1,31 @@
 import telebot
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
-# url = 'https://bujan.ru/tainstvo/tolkovaniya/sonnik/'
-#
-# page = requests.get(url)
-#
-# soup = BeautifulSoup(page.text, 'lxml')
-#
-# words = soup.find('div', class_='tpl-block-list-objects tpl-block-171-list')
-# print(words.text.strip())
-
+url = ''
 bot = telebot.TeleBot('')
 
-@bot.message_handler(commands=['start'])
+
+def search(text):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'lxml')
+    words = soup.find('div', class_='tpl-block-list-objects tpl-block-171-list')
+    data = words.text.strip().split('\n')
+    for line in data:
+        if text.lower() in line.lower():
+            return line
+    return 'Не найдено'
+
+
+@bot.message_handler(content_types=['text'])
 def start(message):
-    bot.send_message(message.char.id, 'Hello')
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, 'Сонник')
+    else:
+        chat_id = message.from_user.id
+        text = message.text
+        response = search(text=text)
+        bot.send_message(chat_id, response)
+
 
 bot.polling(none_stop=True)
