d9819dd006c3ae68186ec574c552391eed805fb4
Роман Ледящев <cruelthem@MacBook-Air-Kirill.local>
2022-05-19 12:53:10 +0300

    Initial commit

---
+++ tgBotTest.py
@@ -0,0 +1,20 @@
+import telebot
+# import requests
+# from bs4 import BeautifulSoup
+
+# url = 'https://bujan.ru/tainstvo/tolkovaniya/sonnik/'
+#
+# page = requests.get(url)
+#
+# soup = BeautifulSoup(page.text, 'lxml')
+#
+# words = soup.find('div', class_='tpl-block-list-objects tpl-block-171-list')
+# print(words.text.strip())
+
+bot = telebot.TeleBot('5108199551:AAHso0Y-wvUoWL_vs6KBv6SjT4OSkAZEkVE')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.char.id, 'Hello')
+
+bot.polling(none_stop=True)
